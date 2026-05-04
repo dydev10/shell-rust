5417da1a8a2dc3496b725723fad06433665c2f08
DY Dev <dydev420@gmail.com>
2026-05-05 04:38:39 +0530

    feat: added echo command

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,18 @@ fn main() {
         // Read command from user
         let mut user_command = String::new();
         match io::stdin().read_line(&mut user_command) {
-            Ok(_) => match user_command.trim() {
-                "exit" => break,
-                _ => println!("{}: command not found", user_command.trim()),
-            },
+            Ok(_) => {
+                let command = user_command.trim();
+
+                if command == "exit" {
+                    break;
+                } else if command.starts_with("echo ") {
+                    let content = command.strip_prefix("echo ").unwrap();
+                    println!("{}", content);
+                } else {
+                    println!("{}: command not found", user_command.trim());
+                }
+            }
             Err(error) => println!("Error {error}"),
         }
     }
