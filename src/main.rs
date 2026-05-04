2705ba3afe711f422cff085fae745152ebb6b1e7
DY Dev <dydev420@gmail.com>
2026-05-05 04:07:09 +0530

    feat: read command from user

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,11 @@ use std::io::{self, Write};
 fn main() {
     print!("$ ");
     io::stdout().flush().unwrap();
+
+    // Read command from user
+    let mut user_command = String::new();
+    match io::stdin().read_line(&mut user_command) {
+        Ok(_) => println!("{}: command not found", user_command.trim()),
+        Err(error) => println!("Error {error}"),
+    }
 }
