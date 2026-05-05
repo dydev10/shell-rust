260ddb3d71666ff15e49d042b19252a9cc6fcd91
DY Dev <dydev420@gmail.com>
2026-05-05 23:23:40 +0530

    fix: temporarily give up windows support

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
-#[cfg(unix)]
+// #[cfg(unix)]
 use std::os::unix::fs::PermissionsExt;
 use std::path::PathBuf;
 
 fn is_exec(path: &PathBuf) -> bool {
-    #[cfg(unix)]
+    // #[cfg(unix)]
     match std::fs::metadata(path) {
         Ok(metadata) => {
             let mode = metadata.permissions().mode();
@@ -22,26 +22,26 @@ fn is_exec(path: &PathBuf) -> bool {
         }
     }
 
-    #[cfg(windows)]
-    {
-        let pathext =
-            std::env::var("PATHEXT").unwrap_or_else(|_| ".COM;.EXE;.BAT;.CMD".to_string());
-        let exec_extensions: Vec<&str> = pathext.split(";").collect();
-        let file_ext = path.extension().and_then(|e| e.to_str());
-        match file_ext {
-            Some(ext) => {
-                let uppercased = ext.to_ascii_uppercase();
-                let ext_with_dot = format!(".{uppercased}");
-                exec_extensions
-                    .iter()
-                    .any(|e| e.eq_ignore_ascii_case(&ext_with_dot))
-            }
-            None => {
-                eprintln!("is_exec:: Can't read extension");
-                false
-            }
-        }
-    }
+    // #[cfg(windows)]
+    // {
+    //     let pathext =
+    //         std::env::var("PATHEXT").unwrap_or_else(|_| ".COM;.EXE;.BAT;.CMD".to_string());
+    //     let exec_extensions: Vec<&str> = pathext.split(";").collect();
+    //     let file_ext = path.extension().and_then(|e| e.to_str());
+    //     match file_ext {
+    //         Some(ext) => {
+    //             let uppercased = ext.to_ascii_uppercase();
+    //             let ext_with_dot = format!(".{uppercased}");
+    //             exec_extensions
+    //                 .iter()
+    //                 .any(|e| e.eq_ignore_ascii_case(&ext_with_dot))
+    //         }
+    //         None => {
+    //             eprintln!("is_exec:: Can't read extension");
+    //             false
+    //         }
+    //     }
+    // }
 }
 
 fn main() {
@@ -72,19 +72,13 @@ fn main() {
                         let mut is_found = false;
                         for path in path_list {
                             let exec_path = path.join(content);
-
-                            let is_present = exec_path.exists();
-                            let is_file = exec_path.is_file();
-                            if is_present && is_file {
-                                let is_executable = is_exec(&exec_path);
-                                if is_executable {
-                                    is_found = true;
-                                    println!("{content} is {}", exec_path.to_str().unwrap());
-                                    break;
-                                }
+                            let is_executable = is_exec(&exec_path);
+                            if is_executable {
+                                is_found = true;
+                                println!("{content} is {}", exec_path.to_str().unwrap());
+                                break;
                             }
                         }
-
                         if !is_found {
                             println!("{}: not found", content);
                         }
